3614e3c9d9794cbb1fb8df5f38e659c75f28d285
Guilherme Biff Zarelli <guilherme.zarelli@luizalabs.com>
2023-01-18 12:01:57 -0300

    fix: web jersey

--- app/spring-app/src/main/java/br/com/helpdev/SpringSampleApplication.java
+++ app/spring-app/src/main/java/br/com/helpdev/SpringSampleApplication.java
@@ -1,11 +1,13 @@
 package br.com.helpdev;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(
     scanBasePackages = {"br.com.helpdev"}
 )
+@EnableAutoConfiguration
 public class SpringSampleApplication {
 
   public static void main(String[] args) {
